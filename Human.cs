0270651d244b8413dbe19ee0b78289b2630d397c
ILMORINEN <moy.morin22@gmail.com>
2023-12-12 00:58:31 +0300

    Create a repository

---
+++ Human.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFPractice
+{
+    public class Human
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? FullName { get; set; }
+        
+        [Required]
+        public DateTime BirthDate { get; set; }
+        
+        [Required]
+        public bool Sex { get; set; }
+    }
+}
---
+++ TaskContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using System.Configuration;
+
+namespace EFPractice
+{
+    public class TaskContext : DbContext
+    {
+
+        public DbSet<Human>? Humans { get; set; }
+        
+        public TaskContext() : base(ConfigurationManager.ConnectionStrings["SQLExpress"].ConnectionString) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
